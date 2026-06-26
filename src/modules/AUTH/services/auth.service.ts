48540ba5d4e03453c53d8b4284ee3cb91696cc86
digbanmije18@gmail.com <digbanmije18@gmail.com>
2026-06-26 22:10:06 +0100

    password mismatch on input field error

--- src/modules/AUTH/services/auth.service.ts
+++ src/modules/AUTH/services/auth.service.ts
@@ -36,10 +36,6 @@ export const registerWorkspace = async (input: CreateWorkspaceInput): Promise<Re
     currency: input.currency,
   })
 
-  if (input.password !== input.confirmPassword) {
-    throw new ConflictError('Passwords do not match')
-  }
-
   const hashPassword = await passwordService.hash(input.password)
 
   const user = await authRepository.createUser({
